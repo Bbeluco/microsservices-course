505867ee0a3798dca0f84a38b4338b53efe5d294
bbeluco <beluco.bispo@hotmail.com>
2023-07-14 12:24:29 -0300

    Private Git repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,5 +1,7 @@
 package com.devsuperior.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
