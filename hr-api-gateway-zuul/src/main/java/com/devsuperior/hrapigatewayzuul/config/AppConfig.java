187a428c1821c7e56ae298832c8c8a0b7e85b66a
bbeluco <beluco.bispo@hotmail.com>
2023-07-25 19:39:06 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -7,10 +8,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${jwt.secret.key}")
+    private String jwtSecretKey;
+
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwt = new JwtAccessTokenConverter();
-        jwt.setSigningKey("MY-SECRET-KEY");
+        jwt.setSigningKey(jwtSecretKey);
         return jwt;
     }
 
